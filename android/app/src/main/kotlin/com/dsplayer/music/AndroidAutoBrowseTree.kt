0d988000f3529a27f92ba66f65dad9f2b51601e7
1525745393 <1525745393@users.noreply.github.com>
2026-06-30 19:44:09 +0000

    feat: Flutter DS Player 复刻
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- android/app/src/main/kotlin/com/dsplayer/music/AndroidAutoBrowseTree.kt
+++ android/app/src/main/kotlin/com/dsplayer/music/AndroidAutoBrowseTree.kt
@@ -5,8 +5,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
-import androidx.media3.common.MediaItem
-import androidx.media3.common.MediaMetadata
 
 /**
  * Android Auto 浏览树
